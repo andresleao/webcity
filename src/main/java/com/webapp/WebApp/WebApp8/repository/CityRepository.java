b0bd3cbba86e886916f1cd77af155337210c3d4c
André Leão <andrefelipesl123@gmail.com>
2021-06-06 13:43:44 -0300

    Projeto criado

---
+++ src/main/java/com/webapp/WebApp/WebApp8/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package com.webapp.WebApp.WebApp8.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webapp.WebApp.WebApp8.model.City;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+
+}
